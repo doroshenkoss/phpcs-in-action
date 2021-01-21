aa3dd85c80a482021db147080ace9b16db27596e
Your Name <you@example.com>
2021-01-21 20:55:58 +0200

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 files=$(git diff-tree --no-commit-id --name-status --diff-filter=d -r "${{ github.event.pull_request.base.sha }"}.."${{ github.event.after }"})
 
-phpcs --standard=phpcs.yml "$files"
+phpcs --standard=phpcs.xml "$files"
