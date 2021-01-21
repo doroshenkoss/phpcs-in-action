349ca11211ea9373e18e679d29db28498c8f76f7
Your Name <you@example.com>
2021-01-21 20:50:40 +0200

    prepare phpcs for latest commit onnly #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
 
-sh -c "phpcs --standard=phpcs.xml $(git diff-tree --no-commit-id --name-status --diff-filter=d -r ${{ github.event.pull_request.base.sha }}..${{ github.event.after }})"
+files=$(git diff-tree --no-commit-id --name-status --diff-filter=d -r "${{ github.event.pull_request.base.sha }"}.."${{ github.event.after }"})
+
+phpcs --standard=phpcs.yml "$files"
